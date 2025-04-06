f03f6a343652e006ede0377ff7a253de6206f820
shayan <shayantrixytb@gmail.com>
2025-04-06 19:56:32 +0330

    Added models

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,8 +1,24 @@
 package models
 
+import(
+	"github.com/google/uuid"
+	"fmt"
+)
+
 type RegisterData struct{
+        ID uuid.UUID    `json:"id"`
         Name string `json:"name"`
         Email string `json:"email"`
         Pass string `json:"password"`
 }
 
+type DataWithoutPass struct{
+        Name string `json:"name"`
+        Email string `json:"email"`
+}
+
+var registerData []RegisterData
+
+func hello(){
+	fmt.Println("Hello")
+}
