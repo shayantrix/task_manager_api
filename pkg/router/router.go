96921a66c94289f9a52d5142de1eaf717f526509
shayan <shayantrixytb@gmail.com>
2025-04-18 12:09:39 +0330

    Added function for completed/incomplete tasks

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -16,7 +16,8 @@ var RoutingGroup = func(router *mux.Router){
 	router.Handle("/protected/taskmanagement/add", middleware.Authorization(http.HandlerFunc(controllers.Add))).Methods("POST")
 	router.Handle("/protected/taskmanagement/delete", middleware.Authorization(http.HandlerFunc(controllers.Delete))).Methods("POST")
 	//router.Handle("/protected/taskmanagement/tasks/{id}", middleware.Authorization(http.HandlerFunc(controllers.GetAllTasks))).Methods("GET")
-	router.Handle("/protected/taskmanagement/update", middleware.Authorization(http.HandlerFunc(controllers.Update))).Methods("PUT")
+	router.Handle("/protected/taskmanagement/update", middleware.Authorization(http.HandlerFunc(controllers.Update))).Methods("PATCH")
 	router.Handle("/protected/taskmanagement/mark", middleware.Authorization(http.HandlerFunc(controllers.Mark))).Methods("POST")
+	router.Handle("/protected/taskmanagement/tasks/{status}", middleware.Authorization(http.HandlerFunc(controllers.TaskRetrieval))).Methods("GET")
 }
 
